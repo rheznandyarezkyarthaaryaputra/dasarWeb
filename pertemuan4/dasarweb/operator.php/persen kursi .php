54777cff58862c45fcc7442a7816e4e3d54f3748
rheznandyarezkyarthaaryaputra <143486697+rheznandyarezkyarthaaryaputra@users.noreply.github.com>
2024-03-05 13:05:37 +0700

    Delete pertemuan4/dasarweb directory

--- pertemuan4/dasarweb/operator.php/persen kursi .php
+++
@@ -1,8 +0,0 @@
-<?php
-$totalKursi = 45;
-$kursiTerisi = 28;
-$kursiKosong = $totalKursi - $kursiTerisi;
-$persentaseKursiKosong = ($kursiKosong / $totalKursi) * 100;
-
-echo "Persentase kursi yang masih kosong adalah: " . $persentaseKursiKosong . "%";
-?>
